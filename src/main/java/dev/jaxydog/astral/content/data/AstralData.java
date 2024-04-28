2ae7c3e2b770bb974ca0d71ffa8a6501f1910b43
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-04-27 22:30:05 -0400

    Document data container

--- src/main/java/dev/jaxydog/astral/content/data/AstralData.java
+++ src/main/java/dev/jaxydog/astral/content/data/AstralData.java
@@ -1,3 +1,17 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * Copyright © 2023–2024 Jaxydog
+ *
+ * This file is part of Astral.
+ *
+ * Astral is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * Astral is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with Astral. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package dev.jaxydog.astral.content.data;
 
 import dev.jaxydog.astral.register.ContentRegistrar;
@@ -7,16 +21,29 @@ import io.github.apace100.calio.data.SerializableDataType;
 
 import java.util.List;
 
-/** Contains shared mod data types */
+/**
+ * Contains definitions of all modded-in data types.
+ *
+ * @author Jaxydog
+ * @since 1.0.0
+ */
 public final class AstralData extends ContentRegistrar {
 
-    /** The moon phase data type */
+    /**
+     * The moon phase data type.
+     *
+     * @since 1.0.0
+     */
     @IgnoreRegistration
     public static final SerializableDataType<MoonPhase> MOON_PHASE = SerializableDataType.enumValue(MoonPhase.class,
         SerializationHelper.buildEnumMap(MoonPhase.class, MoonPhase::getName)
     );
 
-    /** A list of moon phase data types */
+    /**
+     * The moon phase array data type.
+     *
+     * @since 1.0.0
+     */
     @IgnoreRegistration
     public static final SerializableDataType<List<MoonPhase>> MOON_PHASES = SerializableDataType.list(MOON_PHASE);
 
