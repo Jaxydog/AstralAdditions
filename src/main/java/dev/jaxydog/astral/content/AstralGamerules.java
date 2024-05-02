925110079af8325ea4bb3ae3eddbe436ba2e76fc
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-05-02 13:59:23 -0400

    Document gamerules

--- src/main/java/dev/jaxydog/astral/content/AstralGamerules.java
+++ src/main/java/dev/jaxydog/astral/content/AstralGamerules.java
@@ -10,41 +10,74 @@ import net.minecraft.world.GameRules.Category;
 import net.minecraft.world.GameRules.IntRule;
 import net.minecraft.world.GameRules.Key;
 
-/** Contains shared mod gamerules */
+/**
+ * Contains definitions of all modded-in gamerules.
+ *
+ * @author Jaxydog
+ * @since 1.1.0
+ */
 public final class AstralGamerules extends ContentRegistrar {
 
-    /** Defines the gamerule that toggles mob challenge on or off */
+    /**
+     * The gamerule that toggles challenge scaling on or off.
+     *
+     * @since 1.1.0
+     */
     @IgnoreRegistration
     public static final Key<BooleanRule> CHALLENGE_ENABLED = GameRuleRegistry.register("challengeEnabled",
         Category.MOBS,
         GameRuleFactory.createBooleanRule(true)
     );
-    /** Defines the gamerule that configures the challenge chunk step size */
+
+    /**
+     * The gamerule that configures the challenge scaling chunk step size.
+     *
+     * @since 1.1.0
+     */
     @IgnoreRegistration
     public static final Key<IntRule> CHALLENGE_CHUNK_STEP = GameRuleRegistry.register("challengeChunkStep",
         Category.MOBS,
         GameRuleFactory.createIntRule(16)
     );
-    /** Defines the gamerule that configures how many attack points are added every chunk step */
+
+    /**
+     * The gamerule that configures the amount of attack points that are added for each chunk step.
+     *
+     * @since 1.1.0
+     */
     @IgnoreRegistration
     public static final Key<DoubleRule> CHALLENGE_ATTACK_ADDITIVE = GameRuleRegistry.register("challengeAttackAdditive",
         Category.MOBS,
         GameRuleFactory.createDoubleRule(1D)
     );
-    /** Defines the gamerule that configures how many health points are added every chunk step */
+
+    /**
+     * The gamerule that configures the amount of health points that are added for each chunk step.
+     *
+     * @since 1.1.0
+     */
     @IgnoreRegistration
     public static final Key<DoubleRule> CHALLENGE_HEALTH_ADDITIVE = GameRuleRegistry.register("challengeHealthAdditive",
         Category.MOBS,
         GameRuleFactory.createDoubleRule(1D)
     );
-    /** Defines the gamerule that determines whether scaling uses world spawn or 0, 0. */
+
+    /**
+     * The gamerule that determines whether challenge scaling uses the world's spawn or 0, 0.
+     *
+     * @since 1.1.0
+     */
     @IgnoreRegistration
     public static final Key<BooleanRule> CHALLENGE_USE_WORLDSPAWN = GameRuleRegistry.register("challengeUseWorldspawn",
         Category.MOBS,
         GameRuleFactory.createBooleanRule(true)
     );
 
-    /** Defines the gamerule that configures the chance of crafting a reward from currency */
+    /**
+     * The gamerule that configures the chance of receiving a reward from currency conversions.
+     *
+     * @since 1.1.0
+     */
     @IgnoreRegistration
     public static final Key<DoubleRule> CURRENCY_REWARD_CHANCE = GameRuleRegistry.register("currencyRewardChance",
         Category.DROPS,
